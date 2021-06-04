34a139112773ca264b373712022a9a866ae3506e
Joshua <joshuamirigazhi@gmail.com>
2021-06-04 13:15:13 +0100

    first stage

---
+++ calculator.py
@@ -0,0 +1,10 @@
+num1 = input("input first number ")
+num2 = input("input second number")
+result = int(num1) + int(num2)
+
+def cube(num):
+    return num*num*num
+result = cube(4)
+
+print(result)
+
